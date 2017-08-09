8532fb1fa9796f34c90f539ce2b057face66cc57
AlexWetzel <wetzela@live.com>
2017-08-09 15:02:23 -0400

    core game is done. Now on to styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
+//Each word from the word list will be split into an array of its letters, and stored in another array
+function fragmentWords(){
+	for (var i = 0; i < wordList.length; i++) {
+		words.push([]);
+		for (var j = 0; j < wordList[i].length; j++) {
+			var letter = wordList[i].charAt(j);
+			words[i].push(letter);
+		}
+		console.log(words[i]);
+	}
+}
+
 //The next word in the list is selected, the round count goes up, and the guesses and pile gets erased
 function nextRound(round){
 	round++;
@@ -37,32 +49,38 @@ function fillBoard(arr, val){
 
 //When the game starts, the turn count is set to: 10. Win count and loss count are set to 0. Round is set to 1.	
 //Round is equal to the index of the active word minus 1.
-var round = 1;
+var round = 0;
 var win = 0;
 var loss = 0;
 var turn = 10;
 
+//This is used to pause before/after a round.
+var stop = 1;
+
 //Guess includes every letter, pile includes incorrect letters. The pile is what gets displayed.
 var guess = [];
 var pile = [];
+var board = [];
 
-// I'm storing my words as an array of words, which are split into arrays of letters.
-// I may change this later, but this is the first thing I thought of so I'm sticking with it.
-var words = [
-	["a", "p", "p", "l", "e"],
-	["c", "h", "e", "r", "r", "y"],
-	["b", "l", "u", "e", "b", "e", "r", "r", "y"],
-	["s", "t", "r", "a", "w", "b", "e", "r", "r", "y"],
-	["o", "r", "a", "n", "g", "e"]
+var wordList = [
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"indigo",
+	"violet"
 ];
+
+//Empty array for storing "fragmented" words
+var words = [];
 
 //When the user opens the page, the word list is randomized.
 //The program chooses the first word in the list, and the board is filled with underscores to show blank spaces.
+fragmentWords();
 randomSort(words);
-var board = makeBlank(words[round-1].length);
-
-document.getElementById("letters").innerHTML = board.join(" ");
-
+// var board = makeBlank(words[round-1].length);
+// document.getElementById("letters").innerHTML = board.join(" ");
 
 //The user inputs a key, the game checks if the letter is in the selected word.
 
@@ -72,62 +90,78 @@ document.getElementById("letters").innerHTML = board.join(" ");
 	console.log(words[round-1]);
 
 document.getElementById("round").innerHTML = round;
+document.getElementById("message").innerHTML = "Press any key to start!"
 
-// for (document.onkeyup){}
 
 document.onkeyup = function(event) {
 
 	document.getElementById("message").innerHTML = " ";
 
-	var letter = event.key;
-
-	// var check = checkIndex(words[round-1], letter);
-
-	//Check if the user repeats a guess
-	if (guess.indexOf(letter) === -1) {
-
-		guess.push(letter);
-
-		//If the letter is not in the word, the board stays the same, the guessed letter goes in the pile, and the number of turns goes down by 1.
-		//If there are 0 turns left and there are still blank spaces, the user loses the round, and their loss count goes up by 1.
-		if (words[round-1].indexOf(letter) === -1){
-			pile.push(letter);
-			turn--;
-
-			//A bad guess will never result in a win, so the game checks the number of turns to decide whether to end the round.
-			if (turn <= 0){
-				document.getElementById("message").innerHTML = "Game Over";			
-				round = nextRound(round);
-				turn = 10;
-				loss++;
-			}
-		}
-
-		//If the letter is in the word, the game finds every instance of the letter in the word,
-		// and overwrites the corresponding blank spaces.
-		else {
-			fillBoard(words[round-1], letter);
-			document.getElementById("letters").innerHTML = board.join(" ");
-
-			//Once the blank spaces are filled in (if no underscores are detected), the user wins that wound, increasing their win count by 1.
-			if (board.indexOf("_") === -1){
-				document.getElementById("message").innerHTML = "You Win!";
-				round = nextRound(round);
-				turn = 10;
-				win++;			
-			}
-		}
-
+	if (stop === 1) {
+		stop = 0;
+		turn = 10;
+		round = nextRound(round);
+		document.getElementById("letters").innerHTML = board.join(" ");
+		document.getElementById("guess").innerHTML = pile;		
 	}
-	//If the user repeats a guess, they are told, and the turn count does not change.
 	else {
-		document.getElementById("message").innerHTML = "You already guessed that";
+		var letter = event.key;
+
+		// var check = checkIndex(words[round-1], letter);
+
+		//Check if the user repeats a guess
+		if (guess.indexOf(letter) === -1) {
+
+			guess.push(letter);
+
+			//If the letter is not in the word, the board stays the same, the guessed letter goes in the pile, and the number of turns goes down by 1.
+			//If there are 0 turns left and there are still blank spaces, the user loses the round, and their loss count goes up by 1.
+			if (words[round-1].indexOf(letter) === -1){
+				pile.push(letter);
+				turn--;
+
+				document.getElementById("guess").innerHTML = pile;
+
+				//A bad guess will never result in a win, so the game checks the number of turns to decide whether to end the round.
+				if (turn <= 0){
+					document.getElementById("message").innerHTML = "Game Over";			
+					// round = nextRound(round);
+					// turn = 10;
+					loss++;
+					stop = 1;
+				}
+			}
+
+			//If the letter is in the word, the game finds every instance of the letter in the word,
+			// and overwrites the corresponding blank spaces.
+			else {
+				fillBoard(words[round-1], letter);
+
+				//Once the blank spaces are filled in (if no underscores are detected), the user wins that wound, increasing their win count by 1.
+				if (board.indexOf("_") === -1){
+					document.getElementById("message").innerHTML = "You Win!";
+					// round = nextRound(round);
+					// turn = 10;
+					win++;
+					stop = 1;	
+				}
+			}
+		}
+	
+	//If the user repeats a guess, they are told, and the turn count does not change.
+		else {
+			document.getElementById("message").innerHTML = "You already guessed that";
+		}
 	}
+	document.getElementById("letters").innerHTML = board.join(" ");
+
+	document.getElementById("tally").innerHTML = "Wins:" + win + "    Losses: " + loss + "      Guesses Remaining: " + turn;
 
 	console.log("Round: "+ round +"; Turn: " + turn + "; win: " + win + "; Loss: " + loss)
 	console.log("Pile:" + pile);
 	console.log("Guesses:" + guess);
 	console.log(words[round-1]);
+	
 }
 
 
